69fc8bde9b58ed5f90b61b7d17e80ce6f1bde244
Thomas Gunsch <thomas.gunsch@daimler.com>
2021-08-11 17:23:59 +0200

    add jwt encoding

--- README.md
+++ README.md
@@ -1,15 +1,65 @@
-Based on https://github.com/otomato-gh/proxy-wasm-rust/blob/master/Cargo.toml
+# envoy-filter-jwt-claim-to-header
 
+Playground of an envoy HTTP filter written as Rust Wasm plugin. The goal was to learn Rust and 
+Wasm plugins for envoy during a long train trip.
 
+The code and deployment is based on Example https://github.com/otomato-gh/proxy-wasm-rust
+
+## Use case
+
+Some applications could be authorized only by setting specific header-property like `X-WEBAUTH-USER`. 
+This filter offers the possibility to copy a JWT claim to a HTTP header value in the upstream request.
+
+So the following configuration is done in the demo:
+* Validate an JWT, passed as Authorization Bearer token. In the demo against a static key; 
+  in real life this should be done against jwks (https://www.envoyproxy.io/docs/envoy/latest/configuration/http/http_filters/jwt_authn_filter.html)
+* Copy the claim to an header (this wasm filter)
+* Remove the Authorization header for security reasons (https://www.envoyproxy.io/docs/envoy/latest/api-v3/config/route/v3/route.proto.html?highlight=request_headers_to_remove)
+
+## Prerequisite
+
+* [Rust & cargo](https://www.rust-lang.org/tools/install) 
+* [docker](https://docs.docker.com/engine/install/ubuntu/) & [docker-compose](https://docs.docker.com/compose/install/)
+* [Task](https://taskfile.dev) for build and deploy
+
+## Build and run
 
 ```shell
-cargo +nightly build --target=wasm32-unknown-unknown --release
+# build the wasm plugin ./target/wasm32-unknown-unknown/release/hello_wasm.wasm
+task build
+
+# build the docker image hello_wasm_envoy:latest with includes the plugin and envoy-config
+task docker-build
+
+# deploy envoy and httpod backend
+task start
+
+# call with JWT
+export set TOKEN=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJpc3MiOiJkZW1vIn0.BnBTjKUMizZyzaRXUR0epc9fjYyFSjErvY2bw64OKLA
+curl -H "Authorization: Bearer $TOKEN" http://localhost:9211/api/get
+```
+
+The token was created on [jwt.io](https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJpc3MiOiJkZW1vIn0.BnBTjKUMizZyzaRXUR0epc9fjYyFSjErvY2bw64OKLA)
+with payload:
+```json
+{
+  "sub": "1234567890",
+  "name": "John Doe",
+  "iat": 1516239022,
+  "iss": "demo"
+}
 
 ```
+and base64 encoded secret `bXktc2VjcmV0LWtleQo=`
+
 Tutorial https://developpaper.com/extend-envoy-through-wasm-and-rust/
 
-# More docs
-
+# Docs
+* Rust book: https://doc.rust-lang.org/book/ch00-00-introduction.html
+* Cargo book: https://doc.rust-lang.org/cargo/index.html
+* Crate proxy-wasm: https://docs.rs/proxy-wasm/0.1.4/proxy_wasm/
+* envoy http wasm filter: https://www.envoyproxy.io/docs/envoy/latest/configuration/http/http_filters/wasm_filter
+* 
 
 ## Webassembly
 * https://rustwasm.github.io/docs/book/
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,63 @@
-use log::trace;
-use log::debug;
+use std::{fmt, str};
+use std::collections::BTreeMap;
+
+use log::{debug, error, trace};
 use proxy_wasm::traits::*;
 use proxy_wasm::types::*;
+use serde::Deserialize;
+use base64::{ decode_config};
+use serde_json::Value;
 
+//#[derive(Serialize, Deserialize, Clone)]
+#[derive(Clone, Debug, Deserialize, PartialEq)]
+struct Config {
+    claim: String,
+    header: String,
+}
+
+#[cfg(not(test))]
 #[no_mangle]
 pub fn _start() {
     proxy_wasm::set_log_level(LogLevel::Trace);
-    proxy_wasm::set_root_context(|_| -> Box<dyn RootContext> { Box::new(MyRootContext) });
+    proxy_wasm::set_root_context(|_| -> Box<dyn RootContext> {
+        Box::new(JwtHttpFilterRoot {
+            config: None
+        })
+    });
 }
 
-struct MyRootContext;
+struct JwtHttpFilterRoot {
+    config: Option<Config>,
+}
 
-impl Context for MyRootContext {}
+impl Context for JwtHttpFilterRoot {}
 
-impl RootContext for MyRootContext {
+impl RootContext for JwtHttpFilterRoot {
+    fn on_configure(&mut self, _: usize) -> bool {
+        if let Some(config_string) = self.get_configuration() {
+            match serde_json::from_slice(&config_string) {
+                Ok(configuration) => {
+                    //debug!("Read config claim {} to header {}",configuration.claim, configuration.header);
+                    self.config = configuration;
+                    return true;
+                }
+                Err(err) => error!("failed to configure filter: {}", err),
+            }
+        }
+        false
+    }
+
     fn create_http_context(&self, context_id: u32) -> Option<Box<dyn HttpContext>> {
-        Some(Box::new(MyHttpContext { context_id }))
+        match &self.config {
+            Some(config) => {
+                Some(Box::new(JwtHttpFilter {
+                    context_id,
+                    claim: config.claim.clone(),
+                    header: config.header.clone(),
+                }))
+            }
+            None => None
+        }
     }
 
     fn get_type(&self) -> Option<ContextType> {
@@ -23,33 +65,57 @@ impl RootContext for MyRootContext {
     }
 }
 
-struct MyHttpContext {
+struct JwtHttpFilter {
     context_id: u32,
+    claim: String,
+    header: String,
 }
 
-impl Context for MyHttpContext {}
+impl Context for JwtHttpFilter {}
 
-impl HttpContext for MyHttpContext {
+impl HttpContext for JwtHttpFilter {
     fn on_http_request_headers(&mut self, _: usize) -> Action {
-        for (name, value) in &self.get_http_request_headers() {
-            trace!("#{} -> {}: {}", self.context_id, name, value);
+        let token: Option<String> = self.get_http_request_header("Authorization");
+        debug!("request intercepted by the hello_wasm filter");
+
+        // last expression is implicit returned. In case of match, the last command of
+        // every case.
+
+        // Check for Authorization header
+        match token {
+            // If Authorization header available
+            Some(token) => {
+                debug!("Authorization header available | token {}", token);
+                match self.get_claims(token) {
+                    Ok(claims) => {
+                        let value = claims.get(&self.claim).unwrap();
+                        debug!("Got jwt value {} for claim {}", value, self.claim);
+                        self.add_http_request_header(&self.header, value.as_str().unwrap());
+                    }
+                    Err(err) => {
+                        self.send_http_response(
+                            400,
+                            vec![("invalid", "request")],
+                            Some(err.to_string().as_bytes()),
+                        );
+                    }
+                }
+
+                // this is the last expression
+                Action::Continue
+            }
+
+            // Authorization header not available. Send a local response with 403
+            None => {
+                self.send_http_response(
+                    403,
+                    vec![],
+                    Some(b"Access forbidden.\n"),
+                );
+                // this is the last expression
+                Action::Pause
+            }
         }
-
-        self.add_http_request_header("blubb", "downstream");
-
-        /* match self.get_http_request_header(":path") {
-             Some(path) if path == "/hello" => {
-                 self.send_http_response(
-                     200,
-                     vec![("Hello", "World"), ("Powered-By", "proxy-wasm")],
-                     Some(b"Hello, World!\n"),
-                 );
-                 Action::Pause
-             }
-             _ => Action::Continue,
-         }*/
-        // last expression is implicit returned
-         Action::Continue
     }
 
     fn on_http_response_headers(&mut self, _: usize) -> Action {
@@ -63,5 +129,112 @@ impl HttpContext for MyHttpContext {
 
     fn on_log(&mut self) {
         debug!("#{} completed.", self.context_id);
+    }
+}
+
+#[derive(Debug)]
+enum AuthHeaderError {
+    InvalidAuthHeaderError,
+    JwtTokenError(String),
+}
+
+impl std::error::Error for AuthHeaderError {}
+
+impl fmt::Display for AuthHeaderError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            AuthHeaderError::InvalidAuthHeaderError => write!(f, "invalid auth header"),
+            AuthHeaderError::JwtTokenError(error) => write!(f, "invalid jwt auth header {}", error),
+        }
+    }
+}
+
+impl JwtHttpFilter {
+    fn get_claims(&self, token_string: String) -> Result<BTreeMap<String, Value>, AuthHeaderError> {
+        match self.read_jwt(token_string) {
+            Ok(jwt_string) => {
+                let parse_result: Result<BTreeMap<String, Value>, String> = decode_claims(&jwt_string);
+                match parse_result {
+                    Ok(token) => {
+                        //let claims: BTreeMap<String, String> = token.claims().to_owned();
+                        return Ok(token.to_owned());
+                    }
+                    Err(err) => Err(AuthHeaderError::JwtTokenError(err))
+                }
+            }
+            Err(err) => Err(err)
+        }
+    }
+
+
+    fn read_jwt(&self, token: String) -> Result<String, AuthHeaderError> {
+        if !token.starts_with("Bearer ") {
+            return Err(AuthHeaderError::InvalidAuthHeaderError);
+        }
+        Ok(token.trim_start_matches("Bearer ").to_owned())
+    }
+}
+
+
+
+// fn decode_claims2(token_string: &str) -> Result<BTreeMap<String, String>, String> {
+//     match token_string.split_once('.') {
+//         Some((_header, message)) => {
+//             match message.split_once('.') {
+//                 Some((claims, _signature)) => {
+//                     println!("hallo: {}", claims);
+//                     let claim_map = BTreeMap::new();
+//                     return Ok(claim_map)
+//                 }
+//                 None => Err("Invalid JWT: No signature found".into())
+//             }
+//         }
+//         None => Err("Invalid JWT: No header found".into())
+//     }
+//
+// }
+
+fn decode_claims(token_string: &str) -> Result<BTreeMap<String, Value>, String> {
+    let result = token_string.split_once('.');
+    if result.is_none() {
+        return Err("Invalid JWT: No header found".into());
+    }
+    let (_header, message) = result.unwrap();
+
+    let result = message.split_once('.');
+    if result.is_none() {
+        return Err("Invalid JWT: No signature found".into());
+    }
+    let (claims, _signature) = result.unwrap();
+
+    let decoded_claims = String::from_utf8(decode_config(claims, base64::URL_SAFE_NO_PAD).unwrap());
+    if decoded_claims.is_err() {
+        return Err("utf8 decode failure".into())
+    }
+
+    let result = serde_json::from_str(&decoded_claims.unwrap());
+    if result.is_err() {
+        return Err("serializing failure".into())
+    }
+    let claim_map: BTreeMap<String, serde_json::Value> = result.unwrap();
+    Ok(claim_map)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{decode_claims};
+
+    #[test]
+    fn test_decode_claims() {
+        let jwt_string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJpc3MiOiJkZW1vIn0.BnBTjKUMizZyzaRXUR0epc9fjYyFSjErvY2bw64OKLA";
+        let result = decode_claims(jwt_string);
+        assert_eq!(result.is_ok(),true);
+        let claims = result.unwrap();
+
+        assert_eq!(claims.get("sub").unwrap(),"1234567890");
+        assert_eq!(claims.get("name").unwrap(),"John Doe");
+        assert_eq!(claims.get("iat").unwrap(),1516239022);
+        assert_eq!(claims.get("iss").unwrap(),"demo");
+
     }
 }
